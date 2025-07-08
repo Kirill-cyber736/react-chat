15a4e3f90e48f5c9b080234e9e0743f3a625f25c
RenatAllakhyarov <rembo.909@yandex.ru>
2025-07-08 16:16:07 +0300

    update WebSocket connection handling and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import App from './components/App/index.tsx'
-import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
-import "./main.css"
+import App from "./components/App/index.tsx";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import "./main.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+    // <StrictMode>
     <App />
-  </StrictMode>,
-)
+    // </StrictMode>,
+);
